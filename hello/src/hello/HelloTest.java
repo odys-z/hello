ffb613120ffd1399450acacd617611956cb570dc
ody <ody@host1>
2018-10-22 00:08:00 +0800

    Adding Junit

---
+++ hello/src/hello/HelloTest.java
@@ -0,0 +1,20 @@
+package hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testLogi() {
+		// fail("Not yet implemented");
+		assertTrue(true, "assertTrue message ... ... ... ...");
+	}
+
+}
