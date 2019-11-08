30800f2e5a5da17821c0dd2a60f08d15aa6ca1a7
odysz <odysseusj@163.com>
2019-11-08 21:46:20 +0800

    hello

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.ArrayList;
+
 public class Hello {
 
 	public static void main(String[] args) {
@@ -9,6 +11,8 @@ public class Hello {
 
 	public static void logi(String msg) {
 		System.out.println(msg);
+		
+		ArrayList<Integer> s = new ArrayList<Integer>(3);
 	}
 
 }
