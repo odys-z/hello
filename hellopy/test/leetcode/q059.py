0875fa9b96d4765b7bacaa1ece3e6f3b3c4594bf
odysz <odysseusj@163.com>
2019-11-13 21:38:55 +0800

    26ms

--- hellopy/test/leetcode/q059.py
+++ hellopy/test/leetcode/q059.py
@@ -1,3 +1,12 @@
+'''
+Created on 10 Nov 2019
+
+https://leetcode.com/problems/spiral-matrix-ii/
+26ms
+
+@author: ody
+'''
+import unittest
 
 def odds(n):
     odds = [2 * n + 1 for n in range(n ** 2)]
@@ -5,30 +14,40 @@ def odds(n):
     for odd in odds:
         n2 += odd
         yield n2
+
+def inc(n):
+    n = 0
+    while True:
+        n += 1
+        yield n
         
-def peerMat(n, mat, sqrs, u, d, l, r):
-    for cx in range(l, r - 1):
-        print('up row', (u, cx))
+def peelMat(n, mat, sqrs, u, d, l, r):
+    if (u == d and l == r):
+        mat[u * n + l] = next(sqrs)
+        return
+
+    for cx in range(l, r):
+        # print('up row', (u, cx))
         # mat[u][cx] = next(sqrs)
         mat[u * n + cx] = next(sqrs)
         
     for rx in range(u, d):
-        print('right col', (u, cx))
+        # print('right col', (rx, r))
         # mat[rx][r] = next(sqrs)
         mat[rx * n + r] = next(sqrs)
 
-    for cx in range(r - 1, l):
-        print('dow row', (d, cx))
+    for cx in range(r, l, -1):
+        # print('dow row', (d, cx))
         # mat[d][cx] = next(sqrs)
         mat[d * n + cx] = next(sqrs)
         
-    for rx in range(d - 1, u):
-        print('left col', (u, cx))
+    for rx in range(d, u, -1):
+        # print('left col', (rx, l))
         # mat[rx][l] = next(sqrs)
         mat[rx * n + l] = next(sqrs)
     
     if l + 1 < r:
-        peerMat(n, mat, sqrs, u + 1, d - 1, l + 1, r -1)
+        peelMat(n, mat, sqrs, u + 1, d - 1, l + 1, r -1)
        
 
 class Solution(object):
@@ -43,12 +62,18 @@ class Solution(object):
         
         l = u = 0  # left up
         r = d = n - 1 # right down
-        peerMat(n, mat, odds(n), u, d, l, r)
+        peelMat(n, mat, odds(n), u, d, l, r)
         
-        print(mat)
+        # print(mat)
         return [mat[i : i + n] for i in range(0, len(mat),  n)]
 
 
-s = Solution()
-m = s.generateMatrix(3)
-print(m)
+
+class Test(unittest.TestCase):
+
+    def test059(self):
+        s = Solution()
+        self.assertEqual([[ 1,  4,  9],
+                          [64, 81, 16],
+                          [49, 36, 25]],
+                          s.generateMatrix(3))
