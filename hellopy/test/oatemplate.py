372a1601e4db0b9fbe5c6e07a937124c70fb01cd
odysz <odysseusj@163.com>
2019-11-17 13:49:40 +0800

    .
    Change-Id: I6f58ffec8aae17d36be333fd6fa41fb10677195e

---
+++ hellopy/test/oatemplate.py
@@ -0,0 +1,20 @@
+'''
+Created on 17 Nov 2019
+
+@author: ody
+'''
+import unittest
+
+class Solution(object):
+    pass
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        s = Solution()
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
