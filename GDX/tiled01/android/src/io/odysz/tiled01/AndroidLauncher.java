14c12f778a9456b043c4b7f3c391a2af143b9e85
odysz <odysseusj@163.com>
2019-11-29 01:23:34 +0800

    .

---
+++ GDX/tiled01/android/src/io/odysz/tiled01/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.odysz.tiled01;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.odysz.tiled01.Tiled01;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Tiled01(), config);
+	}
+}
---
+++ tiled/README.md
@@ -0,0 +1,6 @@
+
+# Resource
+
+[tutorial](https://www.gamefromscratch.com/post/2014/04/15/A-quick-look-at-Tiled-An-open-source-2D-level-editor.aspx)
+
+![tile png](http://lunar.lostgarden.com/uploaded_images/ExteriorTest-760306.jpg)
