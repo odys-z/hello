5e4d1805034fc77fce132727da77d72bf5b613c4
odysz <odysseusj@163.com>
2019-12-04 00:42:26 +0800

    1ms 99.99%

--- hello/src/leetcode/list/q002_add2nums/Solution.java
+++ hello/src/leetcode/list/q002_add2nums/Solution.java
@@ -13,6 +13,11 @@ package leetcode.list.q002_add2nums;
  * Output: 7 -> 0 -> 8
  * Explanation: 342 + 465 = 807.
  * 
+ * Runtime: 1 ms, faster than 99.99% of Java online submissions for Add Two Numbers.
+ * Memory Usage: 43.7 MB, less than 87.15% of Java online submissions for Add Two Numbers.
+ * 
+ * Faster than solution because the remain list when addition finished is not iterated.
+ * 
  * @author ody
  */
 public class Solution {
@@ -20,7 +25,6 @@ public class Solution {
 		int c = 0;
 		ListNode h1 = l1;
 		ListNode r1 = null;
-		ListNode r2 = null;
 		while (l2 != null && l1 != null) {
 			l1.val += l2.val + c;
 			c = l1.val / 10;
@@ -28,7 +32,6 @@ public class Solution {
 			
 			r1 = l1;
 			l1 = l1.next;
-			r2 = l2;
 			l2 = l2.next;
 		}
 		
@@ -37,13 +40,18 @@ public class Solution {
 			l1 = l2;
 		}
 		
-		while (l1 != null) {
+		while (l1 != null && c > 0) {
 			l1.val += c;
 			c = l1.val / 10;
 			l1.val %= 10;
+
+			r1 = l1;
 			l1 = l1.next;
 		}
-			
+		
+		if (c != 0)
+			r1.next = new ListNode(c);
+
 		return h1;
 	}
 }
