0625ede6edfb7c4e7143ec1f892f5fc026a73ff3
odysz <odysseusj@163.com>
2019-12-08 22:56:35 +0800

    .

--- GDX/tiled01/android/assets/t01.tsx
+++ GDX/tiled01/android/assets/t01.tsx
@@ -4,6 +4,19 @@
   <property name="b2d type" type="int" value="0"/>
  </properties>
  <image source="tileset.png" width="512" height="512"/>
+ <tile id="182">
+  <animation>
+   <frame tileid="172" duration="1100"/>
+   <frame tileid="137" duration="1100"/>
+   <frame tileid="171" duration="1100"/>
+   <frame tileid="27" duration="1100"/>
+   <frame tileid="173" duration="1100"/>
+   <frame tileid="29" duration="1100"/>
+   <frame tileid="13" duration="1100"/>
+   <frame tileid="199" duration="1100"/>
+   <frame tileid="168" duration="1100"/>
+  </animation>
+ </tile>
  <tile id="236">
   <animation>
    <frame tileid="167" duration="100"/>
