bd385dfceb2edc8f6da314c3ec5246ff5eaac79f
odysz <odysseusj@163.com>
2020-05-14 01:00:39 +0800

    q010
    Change-Id: Ibd65492941d8a70e060f69b0ce9712ac0e6b682a

--- hello/src/leetcode/regex/Q010.java
+++ hello/src/leetcode/regex/Q010.java
@@ -44,7 +44,15 @@ package leetcode.regex;
  *
  */
 public class Q010 {
-	public boolean isMatch(String s, String p) {
+
+	/**
+	 * Runtime: 69 ms, faster than 10.85% of Java online submissions for Regular Expression Matching.
+	 * Memory Usage: 39.7 MB, less than 37.37% of Java online submissions for Regular Expression Matching.
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public boolean isMatch1(String s, String p) {
 		int slen = s == null ? 0 : s.length();
 		if (p == null || p.length() == 0)
 			return s == null || slen == 0;
@@ -54,8 +62,85 @@ public class Q010 {
 					&& (p0 == s.charAt(0) || p0 == '.');
 		
 		if (p.length() > 1 && p.charAt(1) == '*')
-			return isMatch(s, p.substring(2))
-					|| (m0 && slen > 0 && isMatch(s.substring(1), p));
-		else return m0 && slen > 0 && isMatch(s.substring(1), p.substring(1));
+			return isMatch1(s, p.substring(2))
+					|| (m0 && slen > 0 && isMatch1(s.substring(1), p));
+		else return m0 && slen > 0 && isMatch1(s.substring(1), p.substring(1));
+	}
+
+	/**Remove substring orperations.
+	 * 
+	 * Runtime: 24 ms, faster than 33.31% of Java online submissions for Regular Expression Matching.
+	 * Memory Usage: 38 MB, less than 47.47% of Java online submissions for Regular Expression Matching.
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public boolean isMatch2(String s, String p) {
+		int slen = s == null ? 0 : s.length();
+		int plen = p == null ? 0 : p.length();
+		if (plen == 0)
+			return slen == 0;
+		
+		return match(s, 0, slen, p, 0, plen);
+	}
+	
+	private boolean match(String s, int sx, int slen, String p, int px, int plen) {
+		if (px >= plen) return sx >= slen;
+		if (sx >= slen)
+			// now, px < plen, only ".*.*..." is ok
+			return px + 2 <= plen && p.charAt(px + 1) == '*' && match(s, sx, slen, p, px + 2, plen);
+		char p0 = p.charAt(px);
+
+		boolean m0 = slen > 0 && (p0 == s.charAt(sx) || p0 == '.');
+		
+		if (px + 1 < plen && p.charAt(px + 1) == '*')
+			return match(s, sx, slen, p, px + 2, plen)
+					|| (m0 && match(s, sx+1, slen, p, px, plen));
+		else return
+			m0 && (sx+1 < slen ? match(s, sx+1, slen, p, px+1, plen) : 
+								match("", 0, 0, p, px+1, plen));
+	}
+	
+	private int slen = 0;
+	private int plen = 0;
+	private String s;
+	private String p;
+	private Boolean[][] ij;
+	/**DP
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public boolean isMatch(String s, String p) {
+		slen = s == null ? 0 : s.length();
+		plen = p == null ? 0 : p.length();
+		if (plen == 0)
+			return slen == 0;
+		
+		this.s = s;
+		this.p = p;
+		this.ij = new Boolean[this.slen+1][this.plen+1];
+		return matchDp(0, 0);
+	}
+
+	private boolean matchDp(int i, int j) {
+		if (ij[i][j] == null) {
+			if (j >= plen) return i >= slen;
+			else if (i >= slen)
+				return j+2 <= plen && p.charAt(j+1) == '*' && matchDp(i, j+2);
+			else {
+				char p0 = p.charAt(j);
+				// m0
+				boolean m0 = slen > 0 && (p0 == s.charAt(i) || p0 == '.');
+				
+				if (i+1 < plen && p.charAt(i+1) == '*')
+					ij[i][j] = matchDp(i, j+2)
+								|| m0 && matchDp(i+1, j);
+//				else ij[i][j] = m0 && (i+1 < slen ? matchDp(i+1, j+1) :
+//													matchDp(i, j+1));
+				else ij[i][j] = m0 && matchDp(i+1, j+1);
+			}
+		}
+		return ij[i][j];
 	}
 }
--- hello/test/leetcode/regex/Q010Test.java
+++ hello/test/leetcode/regex/Q010Test.java
@@ -10,12 +10,39 @@ class Q010Test {
 	@Test
 	void test() {
 		Q010 parser = new Q010(); 
+		Assert.assertTrue(parser.isMatch(null, null));
+		Assert.assertTrue(parser.isMatch("", null));
+		Assert.assertTrue(parser.isMatch("", ""));
+		Assert.assertTrue(parser.isMatch("", ".*"));
+		Assert.assertTrue(parser.isMatch(null, ".*"));
+		Assert.assertFalse(parser.isMatch("", "a"));
+		Assert.assertTrue(parser.isMatch("", "a*"));
+		Assert.assertTrue(parser.isMatch("", "c*c*"));
+//		Assert.assertFalse(parser.isMatch1("", "c**"));
+//		Assert.assertFalse(parser.isMatch("", "c**"));
+		Assert.assertFalse(parser.isMatch("a", ""));
 		Assert.assertTrue(parser.isMatch("a", "a"));
 		Assert.assertFalse(parser.isMatch("aa", "a"));
 		Assert.assertTrue(parser.isMatch("ab", ".b"));
+		Assert.assertTrue(parser.isMatch("ab", ".*"));
 		Assert.assertTrue(parser.isMatch("aaa", ".*"));
-		Assert.assertFalse(parser.isMatch("", "a"));
-		Assert.assertTrue(parser.isMatch("", null));
+		Assert.assertTrue(parser.isMatch("aab", "c*a*b"));
+		Assert.assertFalse(parser.isMatch("mississippi", "mis*is*p*."));
+
+		Assert.assertTrue(parser.isMatch("mississippi", "mis*is*ip*."));
+		Assert.assertTrue(parser.isMatch("mississi", "mis*is*i*"));
+		Assert.assertFalse(parser.isMatch("mississi", "mis*is*i."));
+		Assert.assertTrue(parser.isMatch("mississi", "mis*is*i.*"));
+		Assert.assertTrue(parser.isMatch("mississi", "mis*is*i"));
+		Assert.assertTrue(parser.isMatch("{\"\":\"ississ123}", "{\"\":\"is*is*123}"));
+		Assert.assertTrue(parser.isMatch("{\"\":  123}", "{\"\": *123}"));
+		Assert.assertTrue(parser.isMatch("mississi", "...*is*i"));
+		Assert.assertTrue(parser.isMatch("mississi", ".*i.*i.*i.*"));
+		Assert.assertTrue(parser.isMatch("iii", ".*i.*i.*i.*"));
+		Assert.assertTrue(parser.isMatch("1i2ii4", ".*i.*i.*i.*"));
+		Assert.assertTrue(parser.isMatch("1i2i i", ".*i.*i.*i.*"));
+		Assert.assertTrue(parser.isMatch("i i iss", ".*i.*i.*i.*"));
+		Assert.assertFalse(parser.isMatch("i", ".*i.*i"));
 	}
 
 }
