65cfa4e5f496ee4d894ba33369914da9b783cd66
odysz <odysseusj@163.com>
2020-07-18 00:21:32 +0800

    .

--- hellopy/nlp/try-worldfreq.py
+++ hellopy/nlp/try-worldfreq.py
@@ -2,6 +2,22 @@
 https://pypi.org/project/wordfreq/#description
 '''
 
-from wordfreq import word_frequency
+from wordfreq import zipf_frequency, get_frequency_dict
 
-zipf_frequency('frequency', 'en')
+f = zipf_frequency('frequency', 'en')
+print('word: "{0}"\tfrequency: {1}'.format('frequency', f))
+
+d = get_frequency_dict('en', wordlist='best')
+
+f = open('freq.word', 'w')
+
+# frequency > 0.0001, words = 1068
+cnt = 0;
+for w in d:
+    if d[w] > 0.0001:
+        cnt += 1
+        f.write('{0} {1}\n'.format(w, d[w]))
+
+f.close()
+
+print("Writen {} lines into freq.word.".format(cnt))
---
+++ jwi/t02/src/io/odysz/nlp/meta/Keeper.java
@@ -0,0 +1,12 @@
+package io.odysz.nlp.meta;
+
+public class Keeper {
+
+	/**Load word file into sqlite.db.
+	 * @param wordpath
+	 * @param conn
+	 */
+	static void loadFrequency(String wordpath, String conn) {
+		
+	}
+}
