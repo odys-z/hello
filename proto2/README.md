c91286b5dcff77b1c1cf31659b347f4189aa0d3f
odysz <odysseusj@163.com>
2020-07-23 00:45:55 +0800

    .

---
+++ proto2/README.md
@@ -0,0 +1,20 @@
+# Troubleshootings
+
+- update SDK
+
+settings -> System Settings -> Android SDK
+
+Add sites:
+
+```
+http://ubuntu.buct.edu.cn:80
+http://sdk.gdgshanghai.com:8080
+http://mirror.neusoft.edu.cn:80
+```
+
+- gradle locally
+
+File > Settings > Build, Execute, Deployment > Gradle >
+
+Use Local gradle distribution
+
---
+++ proto2/android/src/io/odysz/edu/en/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.odysz.edu.en.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.odysz.edu.en.SceneB2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SceneB2(), config);
+	}
+}
