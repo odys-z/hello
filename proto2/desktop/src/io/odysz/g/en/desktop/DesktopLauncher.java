7f094323e0bd613179e2572b399a3669271e6fb5
odysz <odysseusj@163.com>
2020-08-01 00:52:35 +0800

    gradle 6.5.1 working with gdx-setup project, proto2

---
+++ proto2/desktop/src/io/odysz/g/en/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.odysz.g.en.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.odysz.g.en.SceneB2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SceneB2(), config);
+	}
+}
