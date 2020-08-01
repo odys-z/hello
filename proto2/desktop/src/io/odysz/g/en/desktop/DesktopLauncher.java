7f02a4e48aa8031e0668f29f5b326aa6cef58f45
odysz <odysseusj@163.com>
2020-08-02 00:13:57 +0800

    .

--- proto2/desktop/src/io/odysz/g/en/desktop/DesktopLauncher.java
+++ proto2/desktop/src/io/odysz/g/en/desktop/DesktopLauncher.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.odysz.g.en.SceneB2;
 
+/**Use eclilpse to debug. 
+ * see <a href='https://stackoverflow.com/questions/56305318/could-not-find-or-load-main-class-with-intellij-application-configuration'>
+ * Could not find or load main class with IntelliJ Application configuration</a> and
+ * <a href='https://youtrack.jetbrains.com/issue/IDEA-122904'>AS Issue</a>
+ * 
+ * @author odys-z@github.com
+ *
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
