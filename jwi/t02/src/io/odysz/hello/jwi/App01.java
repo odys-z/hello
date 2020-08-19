18d15e280acf606f10b26736904c4b9f7ec3e1c4
odysz <odysseusj@163.com>
2020-08-19 13:01:10 +0800

    .

--- jwi/t02/src/io/odysz/hello/jwi/App01.java
+++ jwi/t02/src/io/odysz/hello/jwi/App01.java
@@ -6,13 +6,16 @@ import java.net.URL;
 import java.util.List;
 
 import edu.mit.jwi.Dictionary;
-import edu.mit.jwi.IDictionary;
 import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.ISynsetID;
 import edu.mit.jwi.item.IWord;
 import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
+import edu.mit.jwi.item.Pointer;
 
 public class App01 {
+
+	private static Dictionary dict;
 
 	public static void main(String[] args) throws IOException {
 		// construct the URL to the Wordnet dictionary directory
@@ -21,10 +24,19 @@ public class App01 {
 		String path = wnhome + File.separator + "dict-3.1";
 		URL url = new URL ( "file", null, path ) ;
 		// construct the dictionary object and open it
-		IDictionary dict = new Dictionary ( url ) ;
+		dict = new Dictionary ( url ) ;
 		dict.open () ;
-		// look up first sense of the word " dog "
-		IIndexWord idxWord = dict.getIndexWord( "dog" , POS.NOUN ) ;
+		for (int wx = 1; wx < args.length; wx++)
+			showSense0(args[wx]);
+		dict.close();
+	}
+
+	/**Look up first sense of the word.
+	 * @param w0
+	 */
+	private static void showSense0(String w0) {
+		IIndexWord idxWord = dict.getIndexWord( w0 , POS.NOUN ) ;
+		if (idxWord == null) return;
 		List<IWordID> wids = idxWord.getWordIDs();
 		IWordID wordID = wids.get(0) ;
 
@@ -33,12 +45,23 @@ public class App01 {
 		System.out.println ( "Lemma = " + word.getLemma() ) ;
 		System.out.println ( "Gloss = " + word.getSynset().getGloss () ) ;
 		
-		for (IWord w : word.getSynset().getWords()) {
-			System.out.println(w.getLemma());
+		System.out.println ( "Hypernyms = " ) ;
+		List<ISynsetID> hypers = word.getSynset().getRelatedSynsets(Pointer.HYPERNYM);
+		for (ISynsetID h : hypers) {
+			List<IWord> w = dict.getSynset(h).getWords();
+			for (IWord hw : w) 
+				System.out.print("\t" + hw.getLemma());
 		}
+
+		List<IWord> synset = word.getSynset().getWords();
+		System.out.println(String.format(
+				"\n------------------- %s %s synonyms  -----------------",
+				w0, synset.size() ));
+		for (IWord w : synset) {
+			System.out.print(w.getLemma());
+			System.out.print("\t");
+		}
+		System.out.println ( "\n" ) ;
 	}
 
-	IDictionary findeSynset(String word) {
-		return null;
-	}
 }
