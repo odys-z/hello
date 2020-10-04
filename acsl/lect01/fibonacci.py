097b0707452c5b6bb30f574e496a6466ca36f591
odysz <odysseusj@163.com>
2020-10-05 01:06:36 +0800

    ..

--- acsl/lect01/fibonacci.py
+++ acsl/lect01/fibonacci.py
@@ -1,7 +1,6 @@
 
-def add2(n, fseq):
-    print('add2({})'.format(n))
-    fseq[n] = fseq[n - 1] + fseq[n - 2]
+def addTo(m, fb):
+    fb[m] = fb[m - 1] + fb[m - 2]
 
 def main():
     a = None
@@ -9,16 +8,23 @@ def main():
     n = int(n)
     if n > 0:
         a = [0] * n # a quick way to initialize a list of sice n
+        # n = 5
+        # [0, 0, 0, 0, 0]
+
     if n < 2:
         print(1)
         return
 
     a[0] = 1
     a[1] = 1
+    # [1, 1, 0, 0, 0]
 
-    for x in range(2, n): # 2, 3, ... 4 (if N = 5)
-        add2(x, a);
+    for x in range(2, n): # 2, 3, ... 4 (if n = 5)
+        # x = 2, x = 3, x = 4
+        # put a(x) = a(x-1) + a(x-2) 
+        addTo(x, a)
 
     print(a[n-1])
 
 main()
+
