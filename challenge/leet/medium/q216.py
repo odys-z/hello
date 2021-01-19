3746eb86241d42fd64b4104782910a829ece4f7c
odys-z <odys-z@github.com>
2021-01-19 23:27:18 +0800

    .

--- challenge/leet/medium/q216.py
+++ challenge/leet/medium/q216.py
@@ -81,27 +81,30 @@ class SolutionStep2():
         return res
 
 class Solution():
+    '''
+    5.49%
+    '''
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         dp = [set() for _ in range(n+1)]
         du = [False] * (n + 1)
         dp[0].add(())
-        
+        du[0] = True
         def combine(k, target):
             dp[target].add((target,))
 
-            for ni in range(1, target):
+            for ni in range(1, min(target, 10)):
                 if not du[target-ni]:
                     combine(k, target-ni)
                     du[target-ni] = True
 
                 for combo in dp[target-ni]:
-                    if ni not in combo:
+                    if ni not in combo and ni < 10:
                         dp[target].add(tuple(sorted(combo + (ni,))))
     
         combine(k, n)
-        print(k, n)
-        print(dp)
-        print(dp[-1])
+#         print(k, n)
+#         print(dp)
+#         print(dp[-1])
 
         '''
         almost but len != k 
@@ -109,7 +112,7 @@ class Solution():
         '''
         res = []
         for t in dp[-1]:
-            if len(t) == k:
+            if len(t) == k and max(t) < 10:
                 res.append(list(t))
         return res
 
@@ -123,7 +126,8 @@ if __name__ == '__main__':
     t.assertCountEqual([], s.combinationSum3(4, 1))
     t.assertCountEqual([], s.combinationSum3(3, 2))
     t.assertCountEqual([[1,2,3,4,5,6,7,8,9]], s.combinationSum3(9, 45))
-    t.assertCountEqual([[1,5,9],[1,6,8],[2,4,9],[2,5,8],[2,6,7],[3,4,8],[3,5,7],[4,5,6],[1,3,11],[1,4,10],[1,2,12],[2,3,10]],
+    t.assertCountEqual([[1,5,9],[1,6,8],[2,4,9],[2,5,8],[2,6,7],[3,4,8],[3,5,7],[4,5,6]],
+                       #[1,3,11],[1,4,10],[1,2,12],[2,3,10] has number grater than 9
                        s.combinationSum3(3, 15))
     '''
     3
