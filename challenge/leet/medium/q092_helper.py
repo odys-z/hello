ae4ba4170029c2f112734ecb9d048dd094c050b8
odysz <odysseusj@163.com>
2021-01-19 17:34:58 +0800

    092

---
+++ challenge/leet/medium/q092_helper.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def assert6(s):
+    t = TestCase()
+
+    h = ListNode(6)
+    h = ListNode(5, h)
+    h = ListNode(4, h)
+    h = ListNode(3, h)
+    h = ListNode(2, h)
+    h = ListNode(1, h)
+    h = s.reverseBetween(h, 2, 5)
+
+    t.assertEqual(1, h.val)
+    h = h.next
+    t.assertEqual(5, h.val)
+    h = h.next
+    t.assertEqual(4, h.val)
+    h = h.next
+    t.assertEqual(3, h.val)
+    h = h.next
+    t.assertEqual(2, h.val)
+    h = h.next
+    t.assertEqual(6, h.val)
+
+def assert5(s):
+    t = TestCase()
+
+    h = ListNode(5)
+    h = ListNode(4, h)
+    h = ListNode(3, h)
+    h = ListNode(2, h)
+    h = ListNode(1, h)
+    h = s.reverseBetween(h, 2, 5)
+
+    t.assertEqual(1, h.val)
+    h = h.next
+    t.assertEqual(5, h.val)
+    h = h.next
+    t.assertEqual(4, h.val)
+    h = h.next
+    t.assertEqual(3, h.val)
+    h = h.next
+    t.assertEqual(2, h.val)
+
+        
