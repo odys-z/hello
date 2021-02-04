6587dccea4804cfebcf18d43ec69bec3540949b0
Odys Zhou <odys.zhou@gmail.com>
2021-02-04 23:30:54 +0800

    .

--- challenge/leet/medium/q152.py
+++ challenge/leet/medium/q152.py
@@ -1,51 +1,89 @@
 '''
-Created on 16 Jan 2021
+    152. Maximum Product Subarray
+    https://leetcode.com/problems/maximum-product-subarray/
 
-@author: odys-z@github.com
+    Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+    Example 1:
+
+    Input: [2,3,-2,4]
+    Output: 6
+    Explanation: [2,3] has the largest product 6.
+    Example 2:
+
+    Input: [-2,0,-1]
+    Output: 0
+    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+    @author: odys zhou
 '''
+
 from unittest import TestCase
 from typing import List
 
-class SolutionTimeout:
+class SolutionErr:
     def maxProduct(self, nums: List[int]) -> int:
-        def prodct(nums, s, e):
-            p = 1
-            for n in nums[s:e]:
-                p *= n
-            return p
+        maxpos = maxnn = nums[0]
+        for n in nums[1:]:
+            if n == 0:
+                maxpos = max(maxpos, maxnn, 0)
+                maxnn = 0
+            else:
+                maxnn = maxnn * n if maxnn != 0 else n
+                maxpos = max(maxpos, maxnn, n)
+        return max(maxpos, maxnn)
 
-        dp = [-float('inf')] * (len(nums) + 1)
-        mxx = -float('inf')
-        mxxrange = None
-
-        for i in range(1, len(nums) + 1):
-            for j in range(i):
-                mxdp = max(prodct(nums, j, i), dp[i])
-                if mxx < mxdp:
-                    mxx = mxdp
-                    mxxrange = (j, i)
-        print(mxxrange)
-        return mxx
+class Solution1:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMin, curMax = 1, 1
         
+        for n in nums:
+            
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n) 
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+        return res
+
 class Solution:
     '''
-    The trick here is that keeping accumulate the product, value 0 where do the work.
-    Dont' forget the number n itself is max/min after been cleared by zero.
+    66.34% ~ 84.95% 
+    
+    x x  0  y        y                      y ...
+            Ui-1    Ui = max(ui * ni, li * ni, ni)
+            Li-1    Li = min(ui * ni, li * ni, ni)
+    ^  
+    res_k-1
+            ^
+            res_k
+
+    U(i) = [ U(i-1) ] * n[i]
+    L(i) = [ L(i-1) ] * n[i]
     '''
     def maxProduct(self, nums: List[int]) -> int:
+        Ui = Li = res = nums[0]
         
-        curr_max = curr_min = all_max = nums[0]
-        for ele in nums[1:]:  
-            curr_max = max(curr_max * ele, curr_min * ele, ele)
-            curr_min = min(curr_max * ele, curr_min * ele, ele)
-            all_max = max(all_max, curr_max)
-            
-        return all_max
-    
+        for n in nums[1:]:
+            nu, nl = n * Ui, n * Li
+            Ui, Li = max(nu, nl, n), min(nu, nl, n)
+            res = max(res, Ui)
+        return res
+        
 if __name__ == '__main__':
-    
     t = TestCase()
     s = Solution()
-    t.assertEqual(6, s.maxProduct([2,3,-2,4]))
-    
-    print('OK!')
+    t.assertEqual(0, s.maxProduct([0]))
+    t.assertEqual(6, s.maxProduct([2, 3, -2, 4]))
+    t.assertEqual(48, s.maxProduct([2, 3, -2, 4, -1]))
+    t.assertEqual(-1, s.maxProduct([-1]))
+    t.assertEqual(0, s.maxProduct([-1, 0]))
+    t.assertEqual(1, s.maxProduct([-1, 1]))
+    t.assertEqual(1, s.maxProduct([-1, 0, 1]))
+    t.assertEqual(0, s.maxProduct([-2, 0, -1]))
+    t.assertEqual(8, s.maxProduct([-2, 4, -1, 0, 3]))
+    t.assertEqual(105, s.maxProduct([0, -1, -15, 7]))
+    t.assertEqual(24, s.maxProduct([2, -5, -2, -4, 3]))
+
+
+    print('ok!')
