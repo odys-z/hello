14a6264d3059b9fa8716bf80534fda365f74ec9a
Odys Zhou <odys.zhou@gmail.com>
2021-02-09 15:30:40 +0800

    .

--- challenge/leet/easy/q168.py
+++ challenge/leet/easy/q168.py
@@ -43,13 +43,9 @@ class Solution:
 
     26       2*26       2*26+1  3*26        3*26+1  25*26+1  25*26+25  26^2
     A@ -> Z  B@ -> AZ   BA      C@ -> BZ    CA      YA       YY        A@@  -> YZ (Y@ + Z)
-                              = BY + 1                               = YY + 1
-
-    26^3+2*26^2++0
-    A A @ @ -> AZ@
-
-    26^3+2*26^2+26+0
-    A    B      A@ @ -> AAA@A@ -> AAZZ
+                              = BY + 1                               = YY + 1 = YZ  <==> 25 * 26 + 26
+                                                                 A@A = YZ + 1 = A@@ + 2 = ZA  <==> 26^2 + 1 = (26) * 26 + 1
+                                                                                                   A@A         ZA
 
     0 1 2 3 4 5 6 7 8 9 10                            25
     @ A B C D E F G H I J K L M N O P Q R S T U V W X Y 
