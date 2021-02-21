3fc095092520e15403a8e0c38a54dc027d8f7882
odysz <odysseusj@163.com>
2021-02-21 21:56:21 +0800

    .

---
+++ challenge/leet.gcc/q1641/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "q1641.h"
+
+using namespace std;
+
+int main()
+{
+    Solution s;
+//    cout << "1 -  5 : " << s.countVowelStrings(1) << endl;
+//    cout << "2 - 15 : " << s.countVowelStrings(2) << endl;
+    cout << "33 - 66045 : " << s.countVowelStrings(33) << endl;
+    return 0;
+}
