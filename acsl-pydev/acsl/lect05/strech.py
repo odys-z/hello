fa1fe94571bb64e60aeff328b22e57fa78bf1060
odys zhou <odysseusj@163.com>
2021-02-26 17:46:31 +0800

    .

---
+++ acsl-pydev/acsl/lect05/strech.py
@@ -0,0 +1,17 @@
+'''
+Created on 26 Feb 2021
+
+@author: odys-z@github.com
+'''
+
+from unittest import TestCase
+
+class Solution:
+    pass
+
+if __name__ == '__main__':
+    t = TestCase()
+    s = Solution()
+    
+    t.assertEqual('B', s.stretch())
+
