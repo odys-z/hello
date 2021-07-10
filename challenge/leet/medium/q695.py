3a1ff590d5653d4bee7d1dfb72a3ba009a62c574
odysz <odysseusj@163.com>
2021-07-10 15:11:26 +0800

    .

--- challenge/leet/medium/q695.py
+++ challenge/leet/medium/q695.py
@@ -26,50 +26,133 @@ from typing import List
 from unittest import TestCase
 
 class Solution:
+    '''
+    21.01% -> 40.01%
+    '''
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         row = len(grid)
         col = len(grid[0])
+        maxlands = 0
         
         def parentOf(islands, ix):
-            p = islands.get(ix)
-            while islands.has(ix):
-                p = islands.get(ix)
-                if (p[0] * row + p[1] != ix):
-                    ix == p[0] * row + p[1]
-                else:
-                    return p
+            if ix not in islands:
+                return (ix, 1)
+            p, l = islands.get(ix)
+            while p != ix: # {ix 15: p 7}, {7: 7}
+                ix = p
+                p, l = islands.get(ix)
+            return p, l
+
+        def dsu(islands, u, v, maxlands):
+            '''
+                u      u v      v
+                v               u
+            '''
+            pu, cu = parentOf(islands, u)
+            pv, cv = parentOf(islands, v)
             
-            return islands.get(ix)
+            if pu < pv:
+                pu, pv = pv, pu
+            lands = cu + cv
+            islands.update({v: (pu, lands)})
+            islands.update({pu: (pu, lands)})
+            islands.update({pv: (pu, lands)})
+            
+           
+            return max(maxlands, cu + cv)
         
-        def dsu(islands, grid):
-            for r in grid:
-                for c in r:
-                    if grid[r][c] != 1 or islands.has(r * row + c):
-                        continue
-
-                    # new found land
-                    p0 = islands.get(r * row + c)
-                    if not p0:
-                        p0 = (r, c)
-                        islands.update({r * row + c: p0})
-
-                    adjs = [(r-1, c), (r, c-1), (r+1, c), (r, c+1)]
-                    for x, y in adjs:
-                        if 0 <= x < row and 0 <= y < col:
-                            ix = x * row + y
-                            p = parentOf(islands, ix)
-                            islands.update({ix: p})
-                                    
-                                
-
-
         islands = dict() # {ix: parent}
-        dsu(islands, grid)
-        
-        
 
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 0:
+                    continue
+
+                me = r * col + c
+
+                # new found land
+                if me not in islands:
+                    maxlands = max(maxlands, 1)
+                    islands.update({me: (me, 1)})
+
+                adjs = [(r+1, c), (r, c+1)]
+                for x, y in adjs:
+                    if 0 <= x < row and 0 <= y < col and grid[x][y] != 0:
+                        ix = x * col + y
+                        if parentOf(islands, ix)[0] == parentOf(islands, me)[0]:
+                            continue
+                        maxlands = dsu(islands, me, ix, maxlands)
+        return maxlands
 
 if __name__ == '__main__':
     s = Solution()
     
     t = TestCase()
+
+    grid = [[1,0,1],
+            [1,1,1]] 
+    t.assertEqual(5, s.maxAreaOfIsland(grid))
+
+    grid = [[1,0],
+            [1,1]] 
+    t.assertEqual(3, s.maxAreaOfIsland(grid))
+
+    grid = [[1,0,0],
+            [1,1,1]] 
+    t.assertEqual(4, s.maxAreaOfIsland(grid))
+
+    grid = [[0,0,1],
+            [1,1,1]] 
+    t.assertEqual(4, s.maxAreaOfIsland(grid))
+
+    grid = [[1,0,0],
+            [1,1,0]] 
+    t.assertEqual(3, s.maxAreaOfIsland(grid))
+   
+    _ = 0
+    grid = [
+        #0 1 2 3 4 5 6 7 8 9 10  12
+        [_,_,1,_,_,_,_,1,_,_,_,_,_],
+        [_,_,_,_,_,_,_,1,1,1,_,_,_],
+        [_,1,1,_,1,_,_,_,_,_,_,_,_],
+        [_,1,_,_,1,1,_,_,1,_,1,_,_],
+        [_,1,_,_,1,1,_,_,1,1,1,_,_], # 4
+        [_,_,_,_,_,_,_,_,_,_,1,_,_],
+        [_,_,_,_,_,_,_,1,1,1,_,_,_],
+        [_,_,_,_,_,_,_,1,1,_,_,_,_]  ]
+
+    t.assertEqual(6, s.maxAreaOfIsland(grid))
+    
+    grid = [[1]]
+    t.assertEqual(1, s.maxAreaOfIsland(grid))
+
+    grid = [[0]]
+    t.assertEqual(0, s.maxAreaOfIsland(grid))
+
+    grid = [[1 for _ in range(4)] for i in range(3)]
+    t.assertEqual(12, s.maxAreaOfIsland(grid))
+
+    grid = [[0 for _ in range(4)] for i in range(3)]
+    t.assertEqual(0, s.maxAreaOfIsland(grid))
+
+    grid = [
+        #0 1 2 3 4 5 6 7 8 9 10  12
+        [1,_,1,_,1,_,1,_,1,_,1,_,1],
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1],
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1],
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1], # 4
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1],
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1],
+        [_,1,_,1,_,1,_,1,_,1,_,1,_],
+        [1,_,1,_,1,_,1,_,1,_,1,_,1]  ]
+    t.assertEqual(1, s.maxAreaOfIsland(grid))
+
+    print('OK!')
+    
+    
+    
