e4a9dc5f68ff458e52c7b0be11d968615fb3550e
Odys Zhou <odys.zhou@gmail.com>
2021-08-07 16:59:58 +0800

    add comments of usaco/friday

--- acsl-pydev/usaco/chapter01/friday.py
+++ acsl-pydev/usaco/chapter01/friday.py
@@ -14,31 +14,44 @@ class Friday:
         startyear: 1900
         startday: weekday for starting date, e.g. 1 for Monday, January 1, Sunday = 0
         N: years
+        
+        explain:
+        --------
+        1. Find out how many days before each 13th - mode 7 can found the day;
+        2. As calendar is complex, break it down to days/month dictionary and a function return is it a leap year
         '''
+        # hint 1
+        # Days of each month - we now know how many days before 13th since the 1 Jan, 1900
+        #         Dec Jan Feb                                 Nov
+        months = [31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+        leaps  = [31, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+
         def isleap(year):
             return True if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 else False
 
         N = int(N)
-        #         Dec Jan Feb                                 Nov
-        months = [31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-        leaps  = [31, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-        
+
         # weekday counter
         days = [0] * 7
 
-        # frist week
+        # days of frist 13th since starting day
         d13 = startday - 1 + 13
         m = 1
         endMonth = N * 12
 
+        # For each month, find how many days to 13th scence starting day (d13)
+        # It's easy to know the day if mod 7
         while m <= endMonth:
             days[d13 % 7] += 1
             mth = m % 12
             d13 += leaps[mth] if isleap(m // 12 + startyear) else months[mth]
             m += 1
-        
-        # 13th falls on Saturday, Sunday, Monday, Tuesday, ..., Friday.
+
+        # move Saturday the last to the first
+        # question: 13th falls on Saturday, Sunday, Monday, Tuesday, ..., Friday.
         days.insert(0, days.pop(-1))
+
+        # convert integers to str
         return list(map(lambda x: str(x), days))
 
 
