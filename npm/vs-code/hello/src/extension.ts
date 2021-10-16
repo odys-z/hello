c7cedd526ad55644a90e295389ec77fa8a2a10ad
odysz <odysseusj@163.com>
2021-10-17 01:59:45 +0800

    ..

--- npm/vs-code/hello/src/extension.ts
+++ npm/vs-code/hello/src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path = require('path');
 import * as vscode from 'vscode';
+import * as cp from "child_process";
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,6 +23,34 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('hello.py', () => {
+		let scriptpath = context.asAbsolutePath(path.join('res', 'anserv.py'));
+
+		function getDocumentWorkspaceFolder(): string | undefined {
+			const fileName = vscode.window.activeTextEditor?.document.fileName;
+			return vscode.workspace.workspaceFolders
+				?.map((folder) => folder.uri.fsPath)
+				.filter((fsPath) => fileName?.startsWith(fsPath))[0];
+		}
+		let fullFilePath = context.asAbsolutePath(path.join('res', 'anserv.py'));
+		let cmd = `pwd && python3 ${fullFilePath} -w ${getDocumentWorkspaceFolder()} &`;
+		console.log(cmd);
+		vscode.window.showInformationMessage(cmd);
+		new Promise<string>((resolve, reject) => {
+			cp.exec(cmd, (err, out) => {
+				if (err) {
+					vscode.window.showInformationMessage(err.message);
+					return reject(err);
+				}
+				vscode.window.showInformationMessage('ok');
+				return resolve(out);
+			});
+		});
+	});
+
+	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
