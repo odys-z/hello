e20fe8388924dd72225bf1c7a731a16c31183b2d
odysz <odysseusj@163.com>
2021-10-17 00:00:59 +0800

    ..

--- npm/vs-code/hello/src/extension.ts
+++ npm/vs-code/hello/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('hello.hi', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code Extension from Hello!');
+		vscode.window.showInformationMessage('Hello VS Code Extension from Hello!!');
 	});
 
 	context.subscriptions.push(disposable);
