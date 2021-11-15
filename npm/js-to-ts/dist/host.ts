a041884b0292c49af262c224340948c736ea6b11
odys zhou <odysseusj@163.com>
2021-11-15 11:09:57 +0800

    ..

--- npm/js-to-ts/dist/host.ts
+++
@@ -1,3 +0,0 @@
-import { Hello } from './hello';
-
-new Hello().hi("TypeScript");
--- tryjs/promise.js
+++ tryjs/promise.js
@@ -1,13 +1,15 @@
 class Client {
 	post(arg, onok, onerr) {
-		console.log('posting...');
-		setTimeout(() => {onok(arg + 1000)}, 1000);
+		console.log(`post(${arg})`);
+		setTimeout(() => {onok(arg + 2000)}, 2000);
 	}
 
 	postSync(arg) {
 		let me = this;
+		console.log('promise ...');
 		return new Promise((resolve, reject) => {
-			me.post(arg, (v) => {
+			me.post(arg || 'async - await: ', (v) => {
+				console.log('resolving promise, v: ', v);
 				resolve(v);
 			},
 			(err)=>{reject(err);});
