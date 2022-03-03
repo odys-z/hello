84a25d847bf5aeb5ea4e32595d0b5b96dfdf3051
Odys Zhou <odysseusj@163.com>
2022-03-03 03:22:38 -0500

    javax.servlet.* is replaced by jakarta.servlet. see
        https://stackoverflow.com/a/66368511/7362888
        jetty 10+ is no longer using javax.servlet

--- jetty/hell-jetty/src/main/java/io/oz/hello/jetty/ExampleServlet.java
+++ jetty/hell-jetty/src/main/java/io/oz/hello/jetty/ExampleServlet.java
@@ -2,10 +2,10 @@ package io.oz.hello.jetty;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ExampleServlet extends HttpServlet {
 
--- jetty/hell-jetty/src/main/java/io/oz/hello/jetty/StartJettyWebserver.java
+++ jetty/hell-jetty/src/main/java/io/oz/hello/jetty/StartJettyWebserver.java
@@ -1,7 +1,20 @@
 package io.oz.hello.jetty;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import io.odysz.semantic.jprotocol.AnsonBody;
+import io.odysz.semantic.jserv.ServPort;
+import io.oz.album.tier.Albums;
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
 public class StartJettyWebserver {
 
@@ -9,12 +22,44 @@ public class StartJettyWebserver {
         Server server = new Server(8082);
 
         ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+//        server.setHandler(handler);
+//        handler.addServletWithMapping(ExampleServlet.class, "/exam");
+		WebServlet info = Albums.class.getAnnotation(WebServlet.class);
+		handler.addServletWithMapping((Class<? extends Servlet>) Albums.class, info.urlPatterns()[0]);
 
-        handler.addServletWithMapping(ExampleServlet.class, "/*");
+        
+//        WebAppContext wacHandler = new WebAppContext();
+//        wacHandler.setContextPath("/jserv");
+//        wacHandler.setWar("jserv-album");
+//        wacHandler.setConfigurationDiscovered(true);
+
+        WebAppContext wacHandler = new WebAppContext();
+        registerServlets(wacHandler, AnnotateServlet.class);
+        registerServlets(wacHandler, Albums.class);
+
+        wacHandler.addServlet(ExampleServlet.class, "/exam");
+
+        server.setHandler(wacHandler);
+        
         server.start();
         server.join();
-
     }
 
+    private static <R extends AnsonBody> void registerServlets(WebAppContext context, Class<? extends ServPort> type) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		WebServlet info = type.getAnnotation(WebServlet.class);
+		for (String pattern : info.urlPatterns()) {
+			javax.servlet.Servlet servlet = type.getConstructor().newInstance();
+			context.addServlet(new ServletHolder(servlet), pattern);
+		}
+	}
+
+	static void registerServlets(ServletContextHandler context, Class<? extends HttpServlet> type)
+    		throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+
+		WebServlet info = type.getAnnotation(WebServlet.class);
+		for (String pattern : info.urlPatterns()) {
+			HttpServlet servlet = type.getConstructor().newInstance();
+			context.addServlet(new ServletHolder(servlet), pattern);
+		}
+	}
 }
