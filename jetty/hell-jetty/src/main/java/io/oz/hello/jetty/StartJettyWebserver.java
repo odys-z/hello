5610a1fa88f7204b771f07ad7a38c62ec4db28db
Odys Zhou <odysseusj@163.com>
2022-03-04 17:57:24 -0500

    .

--- jetty/hell-jetty/src/main/java/io/oz/hello/jetty/StartJettyWebserver.java
+++ jetty/hell-jetty/src/main/java/io/oz/hello/jetty/StartJettyWebserver.java
@@ -4,14 +4,12 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import io.odysz.semantic.jprotocol.AnsonBody;
 import io.odysz.semantic.jserv.ServPort;
 import io.oz.album.tier.Albums;
-import javax.servlet.Servlet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
@@ -19,18 +17,6 @@ public class StartJettyWebserver {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(8082);
-
-        ServletHandler handler = new ServletHandler();
-//        server.setHandler(handler);
-//        handler.addServletWithMapping(ExampleServlet.class, "/exam");
-		WebServlet info = Albums.class.getAnnotation(WebServlet.class);
-		handler.addServletWithMapping((Class<? extends Servlet>) Albums.class, info.urlPatterns()[0]);
-
-        
-//        WebAppContext wacHandler = new WebAppContext();
-//        wacHandler.setContextPath("/jserv");
-//        wacHandler.setWar("jserv-album");
-//        wacHandler.setConfigurationDiscovered(true);
 
         WebAppContext wacHandler = new WebAppContext();
         wacHandler.setContextPath("/jserv");
@@ -46,7 +32,8 @@ public class StartJettyWebserver {
         server.join();
     }
 
-    private static <R extends AnsonBody> void registerServlets(WebAppContext context, Class<? extends ServPort> type) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+    private static <R extends AnsonBody> void registerServlets(WebAppContext context, Class<? extends ServPort<R>> type)
+    		throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		WebServlet info = type.getAnnotation(WebServlet.class);
 		for (String pattern : info.urlPatterns()) {
 			javax.servlet.Servlet servlet = type.getConstructor().newInstance();
