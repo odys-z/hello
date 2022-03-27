dc064a4af3d4309351861c92750b26476c1db828
Odys Zhou <odys.zhou@gmail.com>
2022-03-27 01:50:26 -0400

    .

---
+++ csharp/MyExplore3/MyExplore3/AppBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using MyExplore3.ViewModels;
+using System.Windows;
+
+namespace MyExplore3
+{
+    class AppBootstrapper: BootstrapperBase
+    {
+        // private IWindowManager winmgr;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        //public AppBootstrapper(IEventAggregator e, IWindowManager mgr)
+        //{
+        //    Initialize();
+        //    this.winmgr = mgr;
+        //}
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // base.OnStartup(sender, e);
+            DisplayRootViewFor<ShellViewModel>();
+            // winmgr.ShowWindowAsync(new ExplorerViewModel((IEventAggregator)e, winmgr, null));
+        }
+
+    }
+}
