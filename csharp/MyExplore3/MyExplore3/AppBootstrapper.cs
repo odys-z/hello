908bf8c98566a640944a628ca4cfc35b03dbf48a
Odys Zhou <odys.zhou@gmail.com>
2022-03-27 22:26:30 -0400

    WPF explore is working...

--- csharp/MyExplore3/MyExplore3/AppBootstrapper.cs
+++ csharp/MyExplore3/MyExplore3/AppBootstrapper.cs
@@ -6,24 +6,14 @@ namespace MyExplore3
 {
     class AppBootstrapper: BootstrapperBase
     {
-        // private IWindowManager winmgr;
-
         public AppBootstrapper()
         {
             Initialize();
         }
 
-        //public AppBootstrapper(IEventAggregator e, IWindowManager mgr)
-        //{
-        //    Initialize();
-        //    this.winmgr = mgr;
-        //}
-
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            // base.OnStartup(sender, e);
             DisplayRootViewFor<ShellViewModel>();
-            // winmgr.ShowWindowAsync(new ExplorerViewModel((IEventAggregator)e, winmgr, null));
         }
 
     }
